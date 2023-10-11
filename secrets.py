2c7c1c5bb42b440f7421b9bfa15d95ce7e8127fc
ochieno eliud <ochienoellie@gmail.com>
2023-10-11 15:16:03 +0300

    Update secrets.py

--- secrets.py
+++ secrets.py
@@ -1,6 +1,6 @@
 # Broker & WiFi authentication tokens
-Wifi_NAME = "peris"
-wifi_PASSWORD = "Perishome"
+Wifi_NAME = "wifi_ssid"
+wifi_PASSWORD = "wifi_password"
 MQTT_HOSTNAME= "03444e7a266c43a9b0b2fd24d1151f8d.s2.eu.hivemq.cloud"
 timezone = "Africa/Nairobi"
 MQTT_USERNAME = "hivemq.webclient.1696430780961"
